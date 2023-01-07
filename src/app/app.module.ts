0adfd19630ede3a5871df2cb254eb0157059a64e
Tomasz Czornak <tomasz.czornak59@gmail.com>
2023-01-07 18:21:21 +0100

    table finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatIconModule} from "@angular/material/icon";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from "./shared/shared.module";
 import { FormsModule } from '@angular/forms';
@@ -14,8 +12,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatIconModule,
     BrowserAnimationsModule,
     SharedModule,
     FormsModule
